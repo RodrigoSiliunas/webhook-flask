9a66fe35d14b225b143b68c1c460a477995d59a3
Rodrigo Siliunas <rodrigo.siliunas98@outlook.com>
2022-05-06 20:33:05 -0300

    :recycle: Adding a route code.

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from pprint import pprint
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
@@ -7,4 +8,22 @@ cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 @app.route('/api/v1/payments', methods=['GET', 'POST'])
 def webhook_payment_check():
-    pass
+    if request.method == 'POST':
+        data = request.get_json()
+        pprint(data)
+
+        return jsonify({
+            "success": {
+                "message": "Pix's status has changed and the payment appears as paid.",
+                "data": data,
+                "code": 201,
+            }
+        }), 201
+
+    return jsonify({
+        "error": {
+            "message": "This route requires calling via a Post method.",
+            "type": "RequestMethodError",
+            "code": 404
+        }
+    }), 404
